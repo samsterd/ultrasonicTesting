468789efb6aa86d595d089432b5a3fcdeff311f6
samsterd <s.h.amsterdam92@gmail.com>
2024-02-20 14:10:48 -0500

    ultratekPulser: commented out legacy code from test_ender

--- ultratekPulser.py
+++ ultratekPulser.py
@@ -1,24 +1,23 @@
 # Code for connecting to an Ultratek CompactPulser, starting and stopping pulses, and disconnecting over a serial port
 # Uses pyserial to connect and send commands. The pulser takes simple commands encoded as ascii ('string'.encode('ascii'))
 
-import numpy as np
 import serial
 
-@dataclass
-class PulserProperties:
-    """Everything needed to configure Ultratek pulser."""
-    damping: str = 'D0'
-    mode: str = 'M0'
-    pulse_voltage: str = 'V300'
-    pulse_width: str = 'W220'
-    pulse_repetition_rate: str = 'P500'
-    mode: str = 'T0'  # internal
-    # LPF: str = 'L2' #low pass filter of 48 MHz
+# @dataclass
+# class PulserProperties:
+#     """Everything needed to configure Ultratek pulser."""
+#     damping: str = 'D0'
+#     mode: str = 'M0'
+#     pulse_voltage: str = 'V300'
+#     pulse_width: str = 'W220'
+#     pulse_repetition_rate: str = 'P500'
+#     mode: str = 'T0'  # internal
+#     # LPF: str = 'L2' #low pass filter of 48 MHz
 
 # Opens pyserial connection to the pulser
 # Input is a string name of the port (i.e. 'COM3')
 # returns a serial class for the connection or -1 if an error occurred
-#TODO: add confirmation, error handling, scanning for proper port
+#TODO: add confirmation, scanning for proper port
 def openPulser(portName):
     #Note pyserial defaults match the ultratek connection parameters
     #For reference these are:
