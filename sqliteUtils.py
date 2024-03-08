f0ec461ae939b14ef08a4fc7272bdbdf32526bf5
samsterd <s.h.amsterdam92@gmail.com>
2024-03-08 09:15:12 -0500

    sqliteUtils: added STA/LTA, plotting of first data set

--- sqliteUtils.py
+++ sqliteUtils.py
@@ -1,10 +1,15 @@
 import sqlite3
-import database as Database
 import numpy as np
 from typing import Callable
 from tqdm import tqdm
+import bottleneck as bn
 import time
-from  matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
+
+
+###############################################################
+#### Basic DB manipulation ###################################
+###############################################################
 
 # TODO: create cursors within functions rather than pass around a global cursor
 # Open a connection to the database specified in filepath and initialize a cursor
@@ -68,13 +73,51 @@ def retrieveParameters(cursor, table = 'parameters'):
 
     return paramDict
 
+# A function to write a data value to a specific row
+# Inputs the connection and cursor objects, the name of the column to insert into, the name of the table
+#     and the column and value to use as the row identifier (should be the PRIMARY KEY of the table)
+# Saves the data to the database and returns nothing
+def writeToDB(con, cur, column : str, table : str, value, keyCol, keyVal):
+
+    # Generate the INSERT query based
+    updateQuery = "UPDATE " + table + " SET " + column + " = " + str(value) + " WHERE " + keyCol + " = " + str(keyVal)
+
+    # Execute and commit the result
+    cur.execute(updateQuery)
+    con.commit()
+
+# Create a new column within a table
+def createNewColumn(con, cur, table : str, columnName : str):
+
+    # Create query from input
+    query = "ALTER TABLE " + table + " ADD COLUMN " + columnName + " REAL"
+
+    cur.execute(query)
+    con.commit()
+
+# delete an existing column in a table. USE WITH CAUTION
+def deleteColumn(con, cur, table: str, columnName: str):
+
+    # Create query from input
+    query = "ALTER TABLE " + table + " DROP COLUMN " + columnName
+
+    cur.execute(query)
+    con.commit()
+
+# TODO: make a search pixels function to SELECT WHERE (pixelrange)
+
+##########################################################################
+########## Data Analysis #################################################
+#########################################################################
+
 # Applies a function to a data set by iterating through it line by line and stores the result
 # Inputs: database cursor, the function to apply to the dataset, the name of the column to store the result in
 #   the column names to retrieve the data from (a list of strings), and the name of the table to retrieve from
 # NOTE: func is assumed to operate on a list of two arrays i.e. [ ([x1,x2,...]), ([y1,y2,...])]
 # Outputs: the result of applying the function to all rows as a list. It also writes the result into a new column in the database
 # TODO: this will probably be faster if the write call is saved for the end
-def applyFunctionToData(connection, cursor, func : Callable, resName : str, dataColumns = ['voltage', 'time'], keyColumn = ['collection_index'], table = 'acoustics'):
+# TODO: this function has become bloated and might need to be broken up
+def applyFunctionToData(connection, cursor, func : Callable, resName : str, dataColumns = ['time', 'voltage'], keyColumn = ['collection_index'], table = 'acoustics', *funcArgs):
 
     # Gather the number of rows in the db
     numRows = numberOfRows(cursor, table)
@@ -108,49 +151,13 @@ def applyFunctionToData(connection, cursor, func : Callable, resName : str, data
         # Retrieve the primary key value of the row as the last member
         keyValue = row[-1]
 
-        funcResult = func(arrayList)
+        funcResult = func(arrayList, *funcArgs)
 
         writeToDB(connection, writeCursor, resName, table, funcResult, keyColumn[0], keyValue)
 
         funcResultList.append(funcResult)
 
     return funcResultList
-
-
-# A function to write a data value to a specific row
-# Inputs the connection and cursor objects, the name of the column to insert into, the name of the table
-#     and the column and value to use as the row identifier (should be the PRIMARY KEY of the table)
-# Saves the data to the database and returns nothing
-def writeToDB(con, cur, column : str, table : str, value, keyCol, keyVal):
-
-    # Generate the INSERT query based
-    updateQuery = "UPDATE " + table + " SET " + column + " = " + str(value) + " WHERE " + keyCol + " = " + str(keyVal)
-
-    # Execute and commit the result
-    cur.execute(updateQuery)
-    con.commit()
-
-# Create a new column within a table
-def createNewColumn(con, cur, table : str, columnName : str):
-
-    # Create query from input
-    query = "ALTER TABLE " + table + " ADD COLUMN " + columnName + " REAL"
-
-    cur.execute(query)
-    con.commit()
-
-# delete an existing column in a table. USE WITH CAUTION
-def deleteColumn(con, cur, table: str, columnName: str):
-
-    # Create query from input
-    query = "ALTER TABLE " + table + " DROP COLUMN " + columnName
-
-    cur.execute(query)
-    con.commit()
-
-# Simple function to take the sum of the absolute values of the 'y' values
-def absoluteSum(arrList):
-    return sum(abs(arrList[1]))
 
 #TODO: make applyFunctionsToData that also iterates through a list of functions
 
@@ -170,9 +177,37 @@ def stringListToArray(strList : str):
 
     return np.array(floatList)
 
+########################################################################
+################## Plotting ############################################
+########################################################################
+
+# plot the first waveform in the db
+def plotWaveform(cursor, xCol = 'time', yCol = 'voltage', table = 'acoustics'):
+
+    # Format the requested columns for the db query
+    columns = xCol + ', ' + yCol
+
+    # Formate and execute SELECt query
+    selectQuery = "SELECT " + columns + " FROM " + table
+
+    rawData = cursor.execute(selectQuery).fetchone()
+
+    xDat = stringListToArray(rawData[0])
+    yDat = stringListToArray(rawData[1])
+
+    plt.plot(xDat, yDat)
+    plt.show()
+
+# TODO: finish writing this once you write a findPixel function
+# def plotPixel(cursor, primaryCoor, secondaryCoor, primaryAxis = 'X', secondaryAxis = 'Z', xCol = 'time', yCol = 'voltage', table = 'acoustics'):
+#
+#     dataColumns = xCol + ', ' + yCol
+#
+#     # Format pixel coordinates as a WHERE statement
+#     wherePixel = 'WHERE ' +
 
 # plot a map
-def plot2DScan(cursor, xCol : str, yCol : str, datCol : str, table = 'acoustics'):
+def plot2DScan(cursor, xCol : str, yCol : str, datCol : str, save = False, fileName = '', table = 'acoustics'):
 
     # Format the requested columns for the db query
     columns = xCol + ', ' + yCol + ', ' + datCol
@@ -192,6 +227,11 @@ def plot2DScan(cursor, xCol : str, yCol : str, datCol : str, table = 'acoustics'
         cDat = np.append(cDat, float(row[2]))
 
     plt.scatter(xDat, yDat, c = cDat)
+    plt.colorbar()
+
+    if save == True:
+        plt.savefig(fileName)
+
     plt.show()
 
     # Make SELECT query
@@ -199,3 +239,56 @@ def plot2DScan(cursor, xCol : str, yCol : str, datCol : str, table = 'acoustics'
     # Convert data to np arrays
 
     # plot
+
+##########################################################################33
+############### Analysis Functions #########################################
+##### For use with applyFunctionToData ###################################
+###########################################################################
+
+# Simple function to take the sum of the absolute values of the 'y' values
+def absoluteSum(arrList):
+    return bn.nansum(abs(arrList[1]))
+
+# Return the  max of the y-values
+def arrayMax(arrList):
+    return bn.nanmax(arrList[1])
+
+# Calculate the time of the first break using STA/LTA algorithm
+# Inputs the arrayList from applyFunctionToData, the length (in number of elements, NOT time) of the short and long averaging window,
+#   and tresholdRatio, a number in (0,1) that determines what fraction of the maximum STA/LTA counts as the first break
+def staltaFirstBreak(arrayList, shortWindow : int, longWindow : int, thresholdRatio = 0.75):
+
+    # Assume that arrayList is (time, voltage) data
+    timeData = arrayList[0]
+    voltageData = arrayList[1]
+
+    staltaArray = stalta(voltageData, shortWindow, longWindow)
+
+    threshold = thresholdRatio * bn.nanmax(staltaArray)
+
+    # Return time where first value in staltaArray is above threshold
+    for i in range(len(staltaArray)):
+        if staltaArray[i] > threshold:
+            return timeData[i]
+
+    # No value was found above threshold. Return -1
+    return -1
+
+###########################################################################
+########## Analysis Helper Functions ####################################
+#### Do not directly use with applyFunctionToData ###########################
+########################################################################
+
+# Calculate STA/LTA for a given short and long window
+# Windows are specified in number of elements (not time)
+# Returns an array of the same length as input. Values within longWindow-1 of the start of the array will be converted to NaNs
+# NOTE: windows are left-handed in this implementation
+def stalta(array, shortWindow, longWindow):
+    # Calculate square of array values
+    arrSquared = array ** 2
+
+    # Calculate moving averages with optimized code from bottleneck package
+    sta = bn.move_mean(arrSquared, shortWindow)
+    lta = bn.move_mean(arrSquared, longWindow)
+
+    return sta / lta
