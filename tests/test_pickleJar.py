c4528ec4502b522cba0c10f275c4991869f7d4a2
samsterd <s.h.amsterdam92@gmail.com>
2024-04-22 14:34:13 -0400

    Added tests for savePickle and loadPickle

--- tests/test_pickleJar.py
+++ tests/test_pickleJar.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import pickle
 # add directory above testing dir (which contains file under test) to sys.path
 sys.path.append(os.path.split(os.path.dirname(__file__))[0])
 import pickleJar as pj
@@ -38,25 +39,54 @@ def test_sqliteToPickle():
 def test_savePickle():
 
     # create a dict to pickle
+    badDictToPickle = {'key' : 1}
 
     # attempt to save without specifying fileName
+    assert pj.savePickle(badDictToPickle) == -1
 
     # specify fileName and save
+    testDir = os.path.dirname(__file__)
+    testFileName = testDir + '\\test_files\\testPickle0.pickle'
+    goodDictToPickle = {'key' : 1, 'fileName' : testFileName}
+    assert pj.savePickle(goodDictToPickle) == 0
 
     # load the pickle and verify data is there
+    loadedPickle = pj.loadPickle(testFileName)
+    assert loadedPickle['key'] == 1
 
     # delete the pickle
-
-    return 0
+    os.remove(testFileName)
 
 # test loadPickle
 def test_loadPickle():
 
     # create a test dict and pickle it
+    testDir = os.path.dirname(__file__)
+    testFileName0 = testDir + '\\test_files\\testPickle0.pickle'
+    testDict = {'key' : 1, 'fileName' : testFileName0}
+    with open(testFileName0, 'wb') as f:
+        pickle.dump(testDict, f)
+    f.close()
 
-    # load the pickle and verify the data is there
+    # load the pickle, verify the data is there, and delete it
+    assert pj.loadPickle(testFileName0)['key'] == 1
+    assert pj.loadPickle(testFileName0)['fileName'] == testFileName0
+    os.remove(testFileName0)
 
-    # pickle a non-dict and load it, verify warning occurs
+    # pickle a non-dict to verify loading will raise proper errors
+    testFileName1 = testDir + '\\test_files\\testPickle1.pickle'
+    testList = [1, 2, 3]
+    with open(testFileName1, 'wb') as f:
+        pickle.dump(testList, f)
+    f.close()
+    loadedList = pj.loadPickle(testFileName1)
+    # test the list loads properly
+    assert testList == loadedList
+    # test that loading printed an warning message
+    printedWarning = capsys.redouterr()
+    assert printedWarning.out == 'loadPickle Warning: loading ' + testFileName1 + ' does not result in a dict. Data manipulation functions and scripts will likely fail.'
+    # clean up the files
+    os.remove(testFileName1)
 
     # manually pickle a dict without 'fileName', load it and verify 'fileName' was added
 
