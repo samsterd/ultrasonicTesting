61f5f8bcf0bea4b6a1090baeab7094ca804f85ce
samsterd <s.h.amsterdam92@gmail.com>
2024-06-06 09:14:40 -0400

    scanner.py: added checkMoveSafety input to scanner.move() to allow overriding safeMoveQ() for the purposes of debugging and using a scanner that has not been homed

--- scanner.py
+++ scanner.py
@@ -67,13 +67,14 @@ class Scanner():
 
     # Writes a series of commands to perform relative movements with the scanner
     # Inputs the axis of motion as a string ('X','Y', or 'Z'), and the distance to move (in mm) (can be negative)
+    #   Also an optional checkMoveSafety flag which should always be set to true unless debugging a scanner that has not been homed
     # function translates the movement to GCode and passes it to the scanner
-    def move(self, axis : str, distance):
+    def move(self, axis : str, distance, checkMoveSafety = True):
 
         if not self.validAxisQ(axis):
             raise ValueError('Input axis is not \'X\', \'Y\', or \'Z\'')
 
-        if not self.safeMoveQ(axis, distance):
+        if checkMoveSafety and not self.safeMoveQ(axis, distance):
             print("Scanner.move Error: input move is not safe. Check that there is enough space for the scanner to make the desired move.")
             return -1
 
