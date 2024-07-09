42284e5e2ccbd41ca816588f92dd847a05cc604c
changlab <changbatterylab@gmail.com>
2024-07-09 15:24:53 -0400

    bending to git's will

--- runUltrasonicExperiment.py
+++ runUltrasonicExperiment.py
@@ -17,7 +17,7 @@ import gui
 
 experimentParams = {
     # If you want to use the GUI, simply set 'gui' : True and the program will guide you through the rest of the setup
-    'gui' : True,
+    'gui' : False,
 
     # What do you want to do?
     #'move' = move the transducers
@@ -27,42 +27,42 @@ experimentParams = {
     #'multi scan' = repeat a 2D scan with a set frequency
     #Once you have selected, fill out the values in the parameter list in the correct section
     #NOTE: ONLY CHANGE THE VALUES AFTER THE COLON ON EACH LINE
-    'experiment' : 'single pulse',
+    'experiment' : 'repeat pulse',
 
     #####################################################################
     ################# 'move' parameters #################################
     ### Only applies when 'experiment' == 'move' ########################
     #####################################################################
 
-    'axis': 'X',                                     # Axis to move scanner
-    'distance': -3,                                  # Distance in mm to move scanner
+    'axis': 'Z',                                     # Axis to move scanner
+    'distance': -5,                                  # Distance in mm to move scanner
 
     #################################################################################
     ###################### Ultrasonic Parameters #####################################
     #### Applies to 'single pulse', 'single scan', and 'multi scan' experiments #####
     #################################################################################
 
-    'transducerFrequency' : 50,                    # Central frequency of the ultrasonic transducer, in MHz. Current options are 2.25 or 50
-    'pulserType' : 'standard',                       # Type of pulser. 'standard' is the single wave CompactPulser. 'tone burst' uses the USBUT350 tone burst pulser
-    'measureTime' : 1,                               # Approx measurement time, in us. Note this can be changed by the picoscope time interval based on samples
+    'transducerFrequency' : 0.5,                    # Central frequency of the ultrasonic transducer, in MHz. Current options are 2.25 or 50
+    'pulserType' : 'tone burst',                       # Type of pulser. 'standard' is the single wave CompactPulser. 'tone burst' uses the USBUT350 tone burst pulser
+    'measureTime' : 480,                               # Approx measurement time, in us. Note this can be changed by the picoscope time interval based on samples
                                                      #      Changes to the measureTime will be printed in the console when the script is run
-    'measureDelay' : 11,                           # Approx delay after trigger to start measuring, in us
+    'measureDelay' : 50,                           # Approx delay after trigger to start measuring, in us
     'voltageRange' : 0.02,                            # Picoscope voltage range in V. Note this is the total range: 1 V = [-0.5 V, 0.5 V]
                                                      # Allowed voltages = (0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20)
     'voltageAutoRange' : True,                      # Set the oscilloscope to rerun measurements where the voltage range of the wave has significantly changed
                                                      # This enables the tightest possible voltageRange to be used, increasing the accuracy of low intensity signals without cutting off high intensity ones
                                                      # NOTE: this can add significant overhead (2-3x increase in collection time) for each waveform where the range changes
     'waves' : 1000,                                  # Number of waves to collect and average
-    'samples': 1000,                                  # Number of data points per wave
-    'halfCycles' : 16,                               # Tone burst pulser only. Number of half-cycles in a tone burst pulse. Minimum 1, maximum 32
+    'samples': 20000,                                  # Number of data points per wave
+    'halfCycles' : 2,                               # Tone burst pulser only. Number of half-cycles in a tone burst pulse. Minimum 1, maximum 32
 
     ################################################################################
     ########################### Saving Names ##########################################
     ##### Applies to 'single scan', 'multi scan', and 'repeat pulse' experiment ####
     ################################################################################
 
-    'experimentFolder': '/home/rpi-001/acoustics/',                     # Name of folder to dump data
-    'experimentName' : 'autorangeTest2',                  # File name for single scan and repeat pulse experiment. Will be appended with .json or .sqlite3
+    'experimentFolder': r'C:\Users\changlab\Desktop\UT Data',                     # Name of folder to dump data
+    'experimentName' : 'Al_T-24hr_dT-30min_halfcy-2_waves-1000_samps-20000_delay-50_measure-480',                  # File name for single scan and repeat pulse experiment. Will be appended with .json or .sqlite3
     'experimentBaseName' : 'test_multiscan_data',   # Base filename for multi scan experiment, which will have the scan # appended to it
     'saveFormat' : 'sqlite',                        # Format to save data. Options are sqlite or json. Sqlite is highly recommended
     'postAnalysis' : False,                         # Option to run simple post-scan analysis and plotting: calculate max-min, STA/LTA, and Hilbert Envelope, save plots of data and dump CSVs of the raw metrics
@@ -78,10 +78,10 @@ experimentParams = {
 
     'primaryAxis' : 'X',                            # First axis of motion for the scan. 'X' is recommended
     'secondaryAxis' : 'Z',                          # Second axis of motion for the scan. 'Z' is recommended
-    'primaryAxisRange' : 20,                         # Distance in mm to scan on the primary axis
-    'primaryAxisStep' : 5,                        # Distance in mm between each scan on the primary axis
-    'secondaryAxisRange' : 20,                       # Distance in mm to scan on the secondary axis
-    'secondaryAxisStep' : -5,                     # Distance in mm between each scan on the secondary axis
+    'primaryAxisRange' : 30,                         # Distance in mm to scan on the primary axis
+    'primaryAxisStep' : 0.5,                        # Distance in mm between each scan on the primary axis
+    'secondaryAxisRange' : 50,                       # Distance in mm to scan on the secondary axis
+    'secondaryAxisStep' : -0.5,                     # Distance in mm between each scan on the secondary axis
 
     ################################################################################
     ################ Repeat Measure Parameters #####################################
@@ -90,15 +90,15 @@ experimentParams = {
 
     'scanInterval' : 3600,                          # For multi scans: Minimum time between start of scans, in seconds.
     'numberOfScans' : 10,                           # For multi scans: Number of times to run the scan
-    'pulseInterval' : 1,                            # For repeat pulse: Minimum time between pulse collection, in seconds
-    'experimentTime' : 5,                           # For repeat pulse: Time to collect data, in seconds
+    'pulseInterval' : 1800,                            # For repeat pulse: Minimum time between pulse collection, in seconds
+    'experimentTime' : 86400,                           # For repeat pulse: Time to collect data, in seconds
 
     #################################################################################
     ########################## Port Names ###########################################
     ######### Only change if instrument USB ports are changed! ######################
     #################################################################################
     'pulserPort' : 'COM5',                          # Ultratek pulser port name
-    'scannerPort' : '/dev/ttyUSB1',                         # Ender port name
+    'scannerPort' : 'COM5',                         # Ender port name
     'dllFile' : 'C://USUTSDK//USBUTSDKC//USBUT.dll',        # Only used for 'pulserType' : 'tone burst'. Location of USBUT350 pulser SDK
 
     ##################################################################################
